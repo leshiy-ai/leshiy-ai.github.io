918663c971f0382e17a9fde262ddbeb591bcb4b7
leshiyalex <leshiyalex@gmail.com>
2026-02-26 07:09:29 +0000

    Secrets and setup environment variables

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
 export const CONFIG = {
     // Твой прокси для Gemini (Cloudflare)
-    GEMINI_PROXY: "https://gemini-proxy.leshiyalex.workers.dev/v1beta",
-    PROXY_SECRET: "j3qq4h7h2v2hch4", // X-Proxy-Secret
+    // В Vite доступ к переменным идет через import.meta.env
+    GEMINI_PROXY: import.meta.env.VITE_GEMINI_PROXY || "https://gemini-proxy.leshiyalex.workers.dev/v1beta",
+    GEMINI_API_KEY: import.meta.env.VITE_GEMINI_API_KEY,
+    PROXY_SECRET: import.meta.env.VITE_PROXY_SECRET,
     
     // Твоя Хранилка (Яндекс)
     STORAGE_GATEWAY: "https://d5dtt5rfr7nk66bbrec2.kf69zffa.apigw.yandexcloud.net",
-    
     // Твой медиа-бот (Cloudflare)
     GEMINI_AI_BOT: "https://leshiy-gemini-bot.leshiyalex.workers.dev"
 };
