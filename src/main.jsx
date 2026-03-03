4dc615a3ddee18cae41a24e4ab6ef606fbf25d09
leshiyalex <leshiyalex@gmail.com>
2026-03-03 03:50:32 +0000

    создаём сайдбар

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,22 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+document.addEventListener('DOMContentLoaded', () => {
+  const sidebar = document.getElementById('sidebar');
+  const toggleBtn = document.getElementById('toggle-menu');
+  const storageBtn = document.getElementById('open-storage');
+
+  // Переключалка меню
+  toggleBtn.addEventListener('click', () => {
+      sidebar.classList.toggle('active');
+  });
+
+  // Обработка клика по сейфу
+  storageBtn.addEventListener('click', () => {
+      alert('Открываем твою хранилку в Яндексе...');
+      // Сюда вставим вызов твоей функции GET /images/{file}
+  });
+});
 
 window.addEventListener('vk-auth-success', (event) => {
   const newUserId = event.detail;
