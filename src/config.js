63f0cf09a5152a85920cb3e7f88147de2969192b
leshiyalex <leshiyalex@gmail.com>
2026-03-29 07:42:16 +0000

    в конфиг добавлен джемини шлюз

--- src/config.js
+++ src/config.js
@@ -21,8 +21,10 @@ export const CONFIG = {
     PROXY_URL: 'https://d5dtt5rfr7nk66bbrec2.kf69zffa.apigw.yandexcloud.net/ai-proxy',
     FALLBACK_PROXY: 'https://leshiy-ai-proxy.leshiyalex.workers.dev',
     GEMINI_PROXY: 'https://gemini-proxy.leshiyalex.workers.dev',
-    // Твоя Хранилка (Яндекс)
+    // Шлюз Хранилки (Яндекс)
     STORAGE_GATEWAY: "https://d5dtt5rfr7nk66bbrec2.kf69zffa.apigw.yandexcloud.net",
+    // Шлюз Gemini-AI Bot (Яндекс)
+    GEMINI_GATEWAY: "https://d5d2v5jjmbggp9k8qe8q.pdkwbi1w.apigw.yandexcloud.net",
     // Твой медиа-бот (Cloudflare)
     GEMINI_AI_BOT: "https://leshiy-gemini-bot.leshiyalex.workers.dev"
 };
