169bea6bcee34543b23c487fbd84035490fd350e
leshiyalex <leshiyalex@gmail.com>
2026-04-01 04:02:19 +0000

    возвращаем фетч в аутх тг

--- src/main.jsx
+++ src/main.jsx
@@ -88,8 +88,8 @@ const tgAppAutoAuth = async () => {
       // Формируем URL
       const returnTo = window.location.origin + window.location.pathname;
       const authUrl = `${CONFIG.STORAGE_GATEWAY}/auth/telegram/callback?bot=gemini&return_to=${encodeURIComponent(returnTo)}&${tg.initData}`;
-      window.location.href = authUrl;
-      /*
+      //window.location.href = authUrl;
+      
       const response = await fetch(authUrl, {
           method: 'GET',
           headers: { 'Accept': 'application/json' }
@@ -102,7 +102,7 @@ const tgAppAutoAuth = async () => {
           localStorage.setItem('vk_user_id', id);
           localStorage.setItem('auth_provider', 'Telegram');
           if (window.fetchUserStatus) window.fetchUserStatus();
-      }*/
+      }
   } catch (err) {
     console.error("Silent auth failed:", err);
   }
