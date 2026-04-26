8a2861df08a3bff8a08c82a9cd00cff326b3b3fe
leshiyalex <leshiyalex@gmail.com>
2026-04-26 05:23:56 +0000

    создание релиза Bubblewrap TWA v1.0.0

---
+++ app/src/main/java/com/leshiy_ai/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.leshiy_ai.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
